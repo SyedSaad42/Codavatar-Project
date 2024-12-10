5cf86153756925c6236dc1b5af1484b08546dc72
SyedSaad42 <160704868+SyedSaad42@users.noreply.github.com>
2024-12-09 22:41:32 -0500

    latest Deploy WorkFlow

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EmailVerification from './components/auth/EmailVerification';
 import BusinessDetails from './components/business/BusinessDetails';
 
 function App() {
   return (
 <BrowserRouter future={{ v7_relativeSplatPath: true }}>
+       <Router basename="/Codavatar-Project">
       <Routes>
         <Route path="/" element={<EmailVerification />} />
         <Route path="/business-details" element={<BusinessDetails />} />
       </Routes>
+      </Router>
     </BrowserRouter>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/Codavatar-Project",
-  server: {
-    port: 5173,
-    open: true
-  }
+  
 })
